f2f899e87e4f1152e6d8020311ef344db82d9d22
oliver.bannister <oliver.bannister@roxorgaming.co.uk>
2020-04-28 20:53:27 +0100

    added package.json script

--- browserPerformance.js
+++ browserPerformance.js
@@ -1,12 +1,17 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+(async () => { // maybe do some sort of logging in thing?
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    const page = await browser.newPage();
+        // await page.tracing.start({ // not sure tracing is needed
+    //     path: 'trace.json',
+    //     categories: ['devtools.timeline']
+    // })
   await page.goto('https://www.bbc.co.uk/news');
 //   await page.type('#password-input', 'nodeExample1!');
 //   await page.click('#submit-button');
 //   await page.click('#nav-menu-item-6837')
+    // await page.tracing.stop();
     
   await page.screenshot({ path: 'screenshot.png' });
 
