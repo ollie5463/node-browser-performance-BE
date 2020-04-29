cb4a1621548f94a1314cac7e869baef781b3977f
oliver.bannister <oliver.bannister@roxorgaming.co.uk>
2020-04-29 18:31:35 +0100

    Running node server using express

--- checkPerformance.js
+++ checkPerformance.js
@@ -1,21 +1,40 @@
 const puppeteer = require('puppeteer');
 const minimist = require('minimist');
+const express = require('express');
 
-(async () => {
+const app = express();
+
+const runTraceAgainstWebsite = (async(website) => {
   const browser = await puppeteer.launch();
     const page = await browser.newPage();
-        await page.tracing.start({ // not sure tracing is needed
+    await page.tracing.start({
         path: 'trace.json',
         categories: ['devtools.timeline']
     })
-    
-    const args = minimist(process.argv.splice(2));
-    
-    const website = args['website'];  
     await page.goto(website);
     await page.tracing.stop();
-    await page.screenshot({ path: 'screenshot.png' });
 
   await browser.close();
-})();
+});
 
+const runMetricAnalysisAgainstWebsite = (async (website) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(website);
+    const metrics = await page.metrics();
+    // console.log(metrics);
+
+  await browser.close();
+  return metrics;
+});
+
+(async () => {
+  const args = minimist(process.argv.splice(2));
+  const website = args['website'];
+  runTraceAgainstWebsite(website);
+  const metrics = await runMetricAnalysisAgainstWebsite(website);
+  // Run node server
+  app.listen(4000, () => {
+    console.log('metrics: ', metrics)
+  })
+})()
